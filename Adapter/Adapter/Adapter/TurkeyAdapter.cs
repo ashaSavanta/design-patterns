855d7107ad09ca28fce54a403a8751eccf2dcc27
ashaSavanta <asha.patel@savanta.com>
2019-07-04 15:51:24 +0100

    Complete Adapter work

---
+++ Adapter/Adapter/Adapter/TurkeyAdapter.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    public class TurkeyAdapter : IDuck
+    {
+        private readonly ITurkey _turkey;
+
+        public TurkeyAdapter(ITurkey turkey)
+        {
+            _turkey = turkey;
+        }
+
+        public void Quack()
+        {
+            _turkey.Gobble();
+        }
+
+        public void Fly()
+        {
+            _turkey.Fly();
+        }
+    }
+}
